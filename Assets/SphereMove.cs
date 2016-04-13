92e4029c11fa339cdd865d3347123ef7af4a6189
jonathan mui <jonathan mui>
2016-04-13 16:28:55 -0700

    more files

---
+++ Assets/SphereMove.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereMove : MonoBehaviour {
+
+    public GameObject camera;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = camera.transform.position;
+	}
+}
